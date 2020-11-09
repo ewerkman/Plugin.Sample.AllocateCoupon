8a03c682b20e4584cdf7ab92b8e0b70ebb2db1ee
Erwin Werkman <werkman.erwin@gmail.com>
2020-11-09 15:26:22 +0100

    First commit

---
+++ ConfigureServiceApiBlock.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Sitecore.Commerce.Core;
+using Sitecore.Framework.Pipelines;
+
+namespace Plugin.Sample.AllocateCoupon
+{
+    public class ConfigureServiceApiBlock : SyncPipelineBlock<ODataConventionModelBuilder, ODataConventionModelBuilder, CommercePipelineExecutionContext>
+    {
+        public override ODataConventionModelBuilder Run(ODataConventionModelBuilder arg, CommercePipelineExecutionContext context)
+        {
+            var allocateCouponAction = arg.Function("AllocateCoupon");
+            allocateCouponAction.Parameter<string>("promotionId");
+            allocateCouponAction.Parameter<string>("groupId");
+            allocateCouponAction.Returns<string>();
+            
+            return arg;
+        }
+    }
+}
---
+++ Controllers/ApiController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Coupons;
+using Sitecore.Commerce.Plugin.Promotions;
+
+namespace Plugin.Sample.AllocateCoupon.Controllers
+{
+    public class ApiController : CommerceODataController
+    {
+        private readonly CommerceCommander _commander;
+        
+        public ApiController(IServiceProvider serviceProvider, CommerceEnvironment globalEnvironment) : base(serviceProvider, globalEnvironment)
+        {
+            _commander = serviceProvider.GetService<CommerceCommander>();
+        }
+        
+        [HttpGet]
+        [ODataRoute("AllocateCoupon(promotionId={promotionId},groupId={groupId})", RouteName = CoreConstants.CommerceApi)]
+        public async Task<IActionResult> AllocateCoupon(string promotionId, string groupId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+
+            // Allocate a new coupon
+            var promotion = await this.Command<NewCouponAllocationCommand>().Process(this.CurrentContext, promotionId, groupId, 1);
+            
+            var promotionCodeGroup = await _commander.GetEntity<PrivateCouponGroup>(this.CurrentContext, groupId);
+            
+            // Get the last promotion code added
+            var couponAllocations = promotionCodeGroup.EntityComponents.OfType<CouponAllocationComponent>().ToList();
+            if (couponAllocations.Any())
+            {
+                var lastCouponAllocation = couponAllocations.Last();
+                return await Task.FromResult( Ok(lastCouponAllocation.Codes.First()));
+            }
+
+            return await Task.FromResult( new NotFoundResult());
+        }
+    }
+}
